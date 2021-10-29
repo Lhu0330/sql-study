65714f13544f52846a45ec5d5f6679844a2c9c69
haun <gkdns12345@daum.net>
2021-10-29 14:15:49 +0900

    10월 29일

--- 관리자계정.sql
+++ 관리자계정.sql
@@ -1,20 +1,31 @@
-select sysdate from dual;
+--select sysdate from dual;
+--
+--ALTER USER HR ACCOUNT UNLOCK IDENTIFIED BY 1234;
+--
+--<<<<<<< HEAD
+--ALTER USER SCOTT ACCOUNT UNLOCK IDENTIFIED BY 1234;
+--=======
+---- scott 계정을 설치 및 비번정하기
+--@C:\oraclexe\app\oracle\product\11.2.0\server\rdbms\admin\scott.sql;
+--alter user scott identified by 1234;
+--<<<<<<< HEAD
+--
+--conn sys as sysdba
+--
+--select * from sys.dba_users;
+--
+--grant create view scott;
+--=======
+-->>>>>>> f5bf472c5af44e771ff58f542a69b06b8de6dab1
+-->>>>>>> f49232470f72c20356721f43266e788915955c6c
 
-ALTER USER HR ACCOUNT UNLOCK IDENTIFIED BY 1234;
+--
+--CREATE USER ORCLSTUDY ACCOUNT UNLOCK IDENTIFIED BY oracle;
+--grant connect, resource, create table to orclstudy; 
+--revoke connect from orclstudy;
+--
+--ALTER USER ORCLSTUDY ACCOUNT UNLOCK IDENTIFIED BY orcl;
+--
+--drop user orclstudy;
 
-<<<<<<< HEAD
-ALTER USER SCOTT ACCOUNT UNLOCK IDENTIFIED BY 1234;
-=======
--- scott 계정을 설치 및 비번정하기
-@C:\oraclexe\app\oracle\product\11.2.0\server\rdbms\admin\scott.sql;
-alter user scott identified by 1234;
-<<<<<<< HEAD
-
-conn sys as sysdba
-
-select * from sys.dba_users;
-
-grant create view scott;
-=======
->>>>>>> f5bf472c5af44e771ff58f542a69b06b8de6dab1
->>>>>>> f49232470f72c20356721f43266e788915955c6c
+grant create view to scott;
